db01d629f8f980b87faa5bbf5b92469334f90e85
vidtv <nikit1534@gmail.com>
2026-09-03 00:27:43 +0200

    Removed unused elements from Article page

--- src/pages/article.page.ts
+++ src/pages/article.page.ts
@@ -1,13 +1,6 @@
 import { expect, Locator, Page } from '@playwright/test';
 import { BasePage } from '@pages/base.page';
 
-export interface PublishedArticleView {
-  title: string;
-  body: string;
-  authorUsername: string;
-  tags: string[];
-}
-
 export class ArticlePage extends BasePage {
   constructor(page: Page) {
     super(page);
@@ -37,15 +30,5 @@ export class ArticlePage extends BasePage {
 
   async deleteArticle(): Promise<void> {
     await this.articleActions.getByRole('button', { name: 'Delete Article' }).click();
-  }
-
-  async expectArticleVisible(article: PublishedArticleView): Promise<void> {
-    await expect(this.getTitleHeading()).toHaveText(article.title);
-    await expect(this.getBodyText(article.body)).toBeVisible();
-    await expect(this.getAuthorLink(article.authorUsername)).toBeVisible();
-
-    for (const tag of article.tags) {
-      await expect(this.getTag(tag)).toBeVisible();
-    }
   }
 }
